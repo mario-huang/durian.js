bc9c630537b37fadce246eb1562ad3bf868920ba
Mario <6395323+mario-huang@users.noreply.github.com>
2024-09-09 22:27:03 +0800

    3000

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
-const PORT = 4000;
+const PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
