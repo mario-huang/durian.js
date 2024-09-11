caf407575b1925e042f78205815a70c0a02023ca
Mario <6395323+mario-huang@users.noreply.github.com>
2024-09-11 14:53:01 +0800

    bench

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # durian.js
 
+## what is durian.js
 I believe that [NestJS](https://github.com/nestjs/nest) is the most powerful and efficient API development framework on Node.js.
 
 durian.js builds on the strengths of NestJS, running on [Bun](https://github.com/oven-sh/bun), and is designed with speed in mind — both in development and runtime performance.
+
+## how fast is durian.js
+
 
--- bench.sh
+++ bench.sh
@@ -1,6 +1,6 @@
 hyperfine --warmup 3 'cd ./nest && npm run build' 'cd ./durian && bun run build'
-hyperfine --warmup 3 'cd ./nest && isDurianBench=true npm run start' 'cd ./durian && isDurianBench=true bun run start'
-hyperfine --warmup 3 'cd ./nest && npm run test' 'cd ./durian && bun run test'
-hyperfine --warmup 3 'cd ./nest && npm run test:cov' 'cd ./durian && bun run test:cov'
+# hyperfine --warmup 3 'cd ./nest && isDurianBench=true npm run start' 'cd ./durian && isDurianBench=true bun run start'
+# hyperfine --warmup 3 'cd ./nest && npm run test' 'cd ./durian && bun run test'
+# hyperfine --warmup 3 'cd ./nest && npm run test:cov' 'cd ./durian && bun run test:cov'
 
-oha -z 10s http://localhost:3000
+# oha -z 10s http://localhost:3000
