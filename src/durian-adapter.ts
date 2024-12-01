9095038f1b9d5180403194ddbc20ee318bf0000f
Mario <6395323+mario-huang@users.noreply.github.com>
2024-12-02 01:33:38 +0800

    update dependencies

--- src/durian-adapter.ts
+++
@@ -1,3 +0,0 @@
-import { AbstractHttpAdapter } from '@nestjs/core/adapters/http-adapter';
-
-class DurianAdapter extends AbstractHttpAdapter {}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-import { FastifyAdapter, NestFastifyApplication } from '../platform-fastify';
 
 const PORT = 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
+  const app = await NestFactory.create(
     AppModule,
-    new FastifyAdapter(),
   );
   await app.listen(PORT);
   Logger.log(`Listening on http://localhost:${PORT} ...`);
